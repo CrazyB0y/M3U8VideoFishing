7bed615a5ae984f1e5e44da1eedafb49a0661031
Anton Zhuchkov <1091822+CrazyB0y@users.noreply.github.com>
2018-11-05 03:09:33 +0300

    Some changes

--- extension/background.js
+++ extension/background.js
@@ -2,15 +2,12 @@ chrome.webRequest.onCompleted.addListener(
 	function(details) {	
 		var url = details.url;
 		console.info("webRequest.onCompleted: " + url); 
-		if(url.indexOf("index.m3u8") !== -1) 
+		if(url.match("http.*?\.m3u8[^&\">]+") )
 		{
-			if(url.indexOf("hls") !== -1) 
-			{
-				//Refresh tab for cancel video playing
-				chrome.tabs.reload(details.tabId, null, function(){});
-				//Open information window with URL
-				chrome.windows.create({url: "page.html?" + url, type: "popup", width: 400, height: 300});
-			}
+			//Refresh tab for cancel video playing
+			chrome.tabs.reload(details.tabId, null, function(){});
+			//Open information window with URL
+			chrome.windows.create({url: "page.html?" + url, type: "popup", width: 400, height: 300});
 		}
 	}, 
 { urls: [ "http://*/*" ], types: ["media", "xmlhttprequest" ] }, ["responseHeaders"]);
